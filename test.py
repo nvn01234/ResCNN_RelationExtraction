f4d12cf5c1149fd46729d736dddf2d1f07c134a4
nhatnv <nguyenvannhat152@gmail.com>
2018-02-25 14:58:38 +0900

    test

--- test.py
+++ test.py
@@ -8,6 +8,8 @@ def test(testing_data, input_x, input_p1, input_p2, s, p, dropout_keep_prob, dat
     y_test = datamanager.generate_y(testing_data)
     y_true = np.argmax(y_test, axis=1)
     scores, pre = sess.run([s, p], {input_x: x_test, input_p1:p1, input_p2:p2, dropout_keep_prob: 1.0})
+    print(np.shape(scores))
+    print(np.shape(pre))
     y_pred = np.argmax(scores)
     f1 = f1_score(y_true, y_pred, average='macro')
     print(f1)
